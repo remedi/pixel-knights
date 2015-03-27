8908116d53cb8945e00b19c6c350349c5796aae2
Jani Kuutvuori <janikuutvuori@hotmail.com>
2015-03-27 17:29:17 +0200

    MM server: Now sends list of connected map servers to client

--- match_making/mm_server.py
+++ match_making/mm_server.py
@@ -2,31 +2,74 @@
 
 import socket
 
-port = 3500
+#Create which contains all the servers and maps they are running. This string is then send to clients.
+def createServerList(serverList):
+  ser_string = ""
+  for server in serverList:
+    ser_string = ser_string + server[0] + " " + server[1]
+  return ser_string
 
-serverAddr = "M4 127.0.0.1 4375"
-
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#Look for open port:
-while True:
-  try:
-    clientSocket.bind(('localhost', port))
-    break
-  except:
-    port += 1
+def getOwnAddr():
+  #We're creating connection to google server, to find out our own IP
+  googleAddr = socket.getaddrinfo('8.8.8.8', 53)
+  #googleAddr = socket.getaddrinfo('www.google.fi', 80)
+  #Choose last tuple from the received list of addresses to get an IPv4 connection
+  googleAddr = googleAddr[-1][-1]
+  #print "Connecting to google server:", googleAddr
+  #Connect to google server
+  ownAddr = socket.create_connection(googleAddr)
+  #Retrieve own IP
+  my_IP = ownAddr.getsockname()[0]
+  print "Retrieved own IP: ", my_IP
+  ownAddr.close()
+  return my_IP
   
+def main():
 
-print "Waiting for client on port: ", clientSocket.getsockname()
-print "Redirection message: ", serverAddr
+  my_IP = getOwnAddr()
+  port = 3500
+  #serverAddr = "M4 127.0.0.1 4375"
+  clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  #All servers that have announced themselves are added to this list:
+  serverList = []
+  
+  #Look for open port:
+  while True:
+    try:
+      clientSocket.bind((my_IP, port))
+      break
+    except socket.error:
+      port += 1
 
-clientSocket.listen(1)
+    
+  
+  print "Waiting for client on port: ", clientSocket.getsockname()
+  #print "Redirection message: ", serverAddr
+  
+  clientSocket.listen(10)
+  
+  while 1:
+  
+    (client, addr) = clientSocket.accept()
+    recv = client.recv(1024)
+    if recv[0] == 'H':
+      print "Client connected from: ", addr
+      if len(serverList) == 0:
+        print "Client connected but serverlist is empty"
+        client.send('E')
+      else:
+        print "Responding with serverlist of %d servers" % len(serverList)
+        sendMe = createServerList(serverList)
+        client.send(sendMe);
+    elif recv[0] == 'S':
+      print "Map server connected: ", addr
+      client.send("O");
+      addrString = addr[0] + " " + str(addr[1])
+      serverList.append([addrString, recv[1]])
+      print "Serverlist is now %d long" % len(serverList)
+    else:
+      print "Got unexpected message from client: %s msg: %s" % (addr, recv)
+    client.close()
 
-while 1:
-
-  (client, addr) = clientSocket.accept()
-  print "Received connection from addr: %s port: %s" % (addr[0], addr[1])
-  client.send(serverAddr);
-  recv = client.recv(1024)
-  print "Got return from client: ", recv
-
+if __name__ == "__main__":
+  main()
