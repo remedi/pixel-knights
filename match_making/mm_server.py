720f0926249f8f9e1d255a6d206e3f5a42bd17a4
Markus Palonen <markus.palonen@aalto.fi>
2015-03-31 13:17:58 +0300

    Matchmaking: Connect straight to the IP

--- match_making/mm_server.py
+++ match_making/mm_server.py
@@ -10,15 +10,9 @@ def createServerList(serverList):
   return ser_string
 
 def getOwnAddr():
-  #We're creating connection to google server, to find out our own IP
-  googleAddr = socket.getaddrinfo('8.8.8.8', 53)
-  #googleAddr = socket.getaddrinfo('www.google.fi', 80)
-  #Choose last tuple from the received list of addresses to get an IPv4 connection
-  googleAddr = googleAddr[-1][-1]
-  #print "Connecting to google server:", googleAddr
   #Connect to google server
   try:
-    ownAddr = socket.create_connection(googleAddr, 5)
+    ownAddr = socket.create_connection(('8.8.8.8', 53), 5)
     #Retrieve own IP
     my_IP = ownAddr.getsockname()[0]
     ownAddr.close()
@@ -32,7 +26,6 @@ def main():
 
   my_IP = getOwnAddr()
   port = 3500
-  #serverAddr = "M4 127.0.0.1 4375"
   clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   #All servers that have announced themselves are added to this list:
   serverList = []
@@ -45,8 +38,6 @@ def main():
     except socket.error:
       port += 1
 
-    
-  
   print "Listening for connection on: ", clientSocket.getsockname()
   #print "Redirection message: ", serverAddr
   
