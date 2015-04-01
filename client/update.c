remedi/pixel-knights

commit 43ddf6472db819b0b5c275930b51cfbc62ed0e5c

Client: Got rid of saving the game state at client side. Close #20

client/update.c
@@ -17,6 +17,9 @@
 #define BUFLEN 1000
 #define MSGLEN 40
 
+#define G_OFFSET_C 1
+#define G_OFFSET_P 4
+
 // Frees memory pointed by 'ptr'. Used as thread cleanup handler
 void free_memory(void *ptr) {
     printf("thread: Freeing my memory\n");
@@ -39,121 +42,65 @@ char **new_message(char *buf, char **msg_array, int msg_count) {
     return msg_array;
 }
 
-// This function is called when G message is received.
-// Parse game status from a character string received from the server.
-Playerdata *initGame(char *buf, Gamedata *game_data, Mapdata map_data) {
-    buf++;
-    int i;
-    int height = map_data.height;
-    int width = map_data.width;
-    Playerdata *players = game_data->players;
-    Playerdata *old_ptr = players;
-    game_data->player_count = *buf;
-    if(game_data->player_count < 1) {
-        printf("Sanity check failed: player_count was: %d\n", game_data->player_count);
-        free(players);
-        return NULL;
-    }
-    buf++;
-    players = realloc(players, game_data->player_count * sizeof(Playerdata));
-    if(players == NULL) {
-        //free(players);
-        free(old_ptr);
-        perror("realloc");
-        return NULL;
-    }
-    for(i = 0; i < game_data->player_count; i++) {
-        players[i].id = *buf;
-        buf++;
-        players[i].x_coord = *buf;
-        if(players[i].x_coord > width || players[i].x_coord < 1) {
-            printf("thread: G message failed sanity check. Player coordinates extend beyond map. X: %d\n", *buf);
-            /*free(players);
-              return NULL;*/
-        }
-        buf++;
-        players[i].y_coord = *buf;
-        if(players[i].y_coord > height || players[i].y_coord < 1) {
-            printf("thread: G message failed sanity check. Player coordinates extend beyond map. Y: %d\n", *buf);
-            /*free(players);
-              return NULL;*/
-        }
-        buf++;
-        players[i].sign = *buf;
-        buf++;
-    }
-    return players;
-}
-
 // This function is a starting point for a thread.
 // The point of this function is to read the messages received from server
 // and to draw the game on the terminal
 void *updateMap(void *ctx) {
-    Gamedata game;
-    Contex_client_thread* c = (Contex_client_thread *) ctx;
-    int *sock_t = c->sock;
+    Context_client_thread* c = (Context_client_thread *) ctx;
     char *map_nr = c->map_nr;
-    Playerdata *players = malloc(sizeof(Playerdata));
     Mapdata map_data;
-    game.players = players;
-    uint8_t x, y;
-    int i, j = 0;
     ssize_t bytes;
     int break_flag = 1;
     char *buf = malloc(sizeof(char) * BUFLEN);
-    char **rows;
     char **message_array;
     int message_count;
-    int *exit_clean = c->main_exit;
+    int i, draw_count = 0;
+
+    // Pointers for iteration
+    uint8_t* player_count,* x,* y;
+    char* sign;
 
     // Initialize mapdata
-    if(createMap(&map_data, *map_nr) == -1) {
-	printf("thread: error with createMap\n");
+    if (createMap(&map_data, *map_nr) == -1) {
+        printf("thread: error with createMap\n");
     }
-    rows = map_data.map;
 
     //Reserve memory for chat service
     message_count = map_data.height;
     message_array = malloc(sizeof(char *) * message_count);
-    for(i = 0; i<message_count; i++) {
+    for (i = 0; i<message_count; i++) {
         message_array[i] = malloc(sizeof(char) * MSGLEN);
         memset(message_array[i], '\0', MSGLEN);
     }
 
-    // Clean message buffer
-    memset(buf, '\0', BUFLEN);
+    printf("thread: Reading socket %d\n", *c->sock);
+    while (break_flag && !*c->main_exit) {
+        // Clean message buffer
+        memset(buf, '\0', BUFLEN);
 
-    //Cleanup handlers for thread cancellation
-    printf("thread: Reading socket %d\n", *sock_t);
-    while(break_flag && ! *exit_clean) {
-        // This cleanup handler needs to be set again for each loop
-        // in case the address of 'players' is changed.
-        bytes = read(*sock_t, buf, BUFLEN);
-        if(bytes < 0) {
+        // Block here until message comes
+        bytes = read(*c->sock, buf, BUFLEN);
+        if (bytes < 0) {
             perror("read");
             continue;
         }
-        else if(bytes == 0) {
+        else if (bytes == 0) {
             printf("thread: Server likely disconnected\n");
             break_flag = 0;
         }
-        else if(buf[0] == 'C') {
+        else if (buf[0] == 'C') {
             printf("thread: Got C message\n");
             new_message(buf+1, message_array, message_count);
         }
-        else if(buf[0] == 'G') {
-            if((bytes - 2) % 4 != 0) {
-                printf("G message failed sanity check. Unaccepted message length: %zu\n", bytes);
-                memset(buf, '\0', BUFLEN);
+        else if (buf[0] == 'G') {
+            if ((bytes - 2) % 4 != 0) {
+                printf("Faulty G message length: %zu\n", bytes);
                 continue;
             }
-            players = initGame(buf, &game, map_data);
-            if(players == NULL) {
-                printf("thread: initGame failed\n");
-                game.player_count = 0;
-            }
-            game.players = players;
-            memset(buf, '\0', BUFLEN);
+            player_count = (uint8_t*) buf+G_OFFSET_C;
+            x = (uint8_t*) buf+G_OFFSET_C+2;
+            y = (uint8_t*) buf+G_OFFSET_C+3;
+            sign = (char*) buf+G_OFFSET_C+4;
         }
         else {
             printf("thread: Received unkown message: %c. Ignoring\n", buf[0]);
@@ -161,38 +108,39 @@ void *updateMap(void *ctx) {
         }
 
         // Only draw map if user isn't busy doing something else
-        if(pthread_mutex_trylock(c->lock) == 0) {
-            system("clear");
-            for(i = 0; i<game.player_count; i++) {
-                //Add players
-                x = players[i].x_coord;
-                y = players[i].y_coord;
-                rows[y][x] = players[i].sign;
-            }
-            printf("\n");
-            for(i = 0; i<map_data.height; i++) {
-                //Actual drawing
-                printf("\t%s", rows[i]);
-                printf("\t\t%s\n", message_array[message_count-i-1]);
-            }
-            printf("\n");
-            for(i = 0; i<game.player_count; i++) {
-                //Undo changes
-                x = players[i].x_coord;
-                y = players[i].y_coord;
-                rows[y][x] = ' ';
-            }
+        pthread_mutex_lock(c->lock);
+        system("clear");
 
-            printf("DEBUG: Drawing loop: %d. Press 'h' for help\n", j);
-            j++;
-            pthread_mutex_unlock(c->lock);
+        // Add players to the map
+        for (i = 0; i < *player_count; i++) {
+            map_data.map[*y][*x] = *sign;
+            x += G_OFFSET_P;
+            y += G_OFFSET_P;
+            sign += G_OFFSET_P;
         }
-        memset(buf, '\0', BUFLEN);
+        for (i = 0; i<map_data.height; i++) {
+            //Actual drawing
+            printf("\t%s", map_data.map[i]);
+            printf("\t\t%s\n", message_array[message_count-i-1]);
+        }
+        printf("\n");
+        x = (uint8_t*) buf+G_OFFSET_C+2;
+        y = (uint8_t*) buf+G_OFFSET_C+3;
+        // Remove players from the map
+        for (i = 0; i < *player_count; i++) {
+            map_data.map[*y][*x] = ' ';
+            x += G_OFFSET_P;
+            y += G_OFFSET_P;
+            sign += G_OFFSET_P;
+        }
+
+        printf("DEBUG: Drawing loop: %d. Press 'h' for help\n", draw_count++);
+        pthread_mutex_unlock(c->lock);
     }
 
     free_memory(buf);
     freeMap(&map_data);
-    //free_memory(players);
+    
     // Free memory allocated for messages
     for(i = 0; i<message_count; i++) {
         free(message_array[i]);
@@ -200,7 +148,6 @@ void *updateMap(void *ctx) {
     free(message_array);
 
     *c->done = 1;
-    free(players);
     printf("thread: Server hung up unexpectedly, exiting\n");
     return 0;
 }

client/update.h
@@ -7,14 +7,10 @@
 // Thread cleanup handler
 void free_memory(void *);
 
-
 // Add the new message contained in buf to the msg_array.
 // Also rotate pointers to make the newest message show as first.
 char **new_message(char *, char **, int); 
 
-// ???
-Playerdata *initGame(char *, Gamedata *, Mapdata);
- 
 // This function is a starting point for a thread.
 // The point of this function is to read the messages received from server
 // and to draw the game on the terminal

client/client.c
@@ -264,7 +264,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Fill the thread context struct and start the thread for updating map
-    Contex_client_thread ctx;
+    Context_client_thread ctx;
     ctx.sock = &sock;
     ctx.lock = &mtx;
     ctx.done = &thread_complete; 

server/main.c
@@ -155,7 +155,7 @@ int main(int argc, char *argv[]) {
     // Initiate thread that keeps sending the clients the game state
     pthread_mutex_t lock;
     pthread_mutex_init(&lock, NULL);
-    Contex_server_thread ctx;
+    Context_server_thread ctx;
     ctx.g = &game;
     ctx.lock = &lock;
     if (pthread_create(&gamestate_thread, NULL, sendGamestate, &ctx) < 0) {

server/thread.c
@@ -20,7 +20,7 @@ void* sendGamestate(void* ctx) {
 
     // Init variables
     char sendbuf[MAXDATASIZE];
-    Contex_server_thread* c = (Contex_server_thread *) ctx;
+    Context_server_thread* c = (Context_server_thread *) ctx;
     Gamestate* g = c->g;
     Gamestate* game = g;
     int status, nbytes;

typedefs.h
@@ -25,38 +25,20 @@ struct gamestate_s {
     Gamestate* next;
 };
 
-//Playerdata on client side. These are about to be removed
-typedef struct playerdata {
-    char name[10];
-    uint8_t id;
-    uint8_t x_coord;
-    uint8_t y_coord;
-    uint8_t hp;
-    uint8_t sign;
-} Playerdata;
-
-// Gamedata includes player PROBABLY NOT NEEDED 
-typedef struct gamedata {
-    uint8_t player_count;
-    uint8_t monster_count;
-    Playerdata *players;
-} Gamedata;
-
-
-// Context structs for client thread
-typedef struct contex_server {
+// Context struct for client thread
+typedef struct context_client {
     int* sock;
     pthread_mutex_t* lock;
     volatile sig_atomic_t* done;
     int *main_exit;
     char *map_nr;
-} Contex_client_thread;
+} Context_client_thread;
 
-// Context structs for server thread
-typedef struct contex_client {
+// Context struct for server thread
+typedef struct context_server {
     Gamestate* g;
     pthread_mutex_t* lock;
-} Contex_server_thread;
+} Context_server_thread;
 
 // Action enumeration
 typedef enum {

commit 9e504f5b869812e326869774e3602658d39c4ef4

Client: Fixed messages segfault caused by commit 43ddf64

client/update.c
@@ -28,18 +28,18 @@ void free_memory(void *ptr) {
 
 // Add the new message contained in buf to the msg_array.
 // Also rotate pointers to make the newest message show as first.
-char **new_message(char *buf, char **msg_array, int msg_count) {
-    int i;
-    char *temp_ptr = msg_array[msg_count-1];
-    //Rearrange pointers
-    for(i = (msg_count-1); i>0; i--) {
-        msg_array[i] = msg_array[i-1];
+void new_message(char *buf, char **msg_array, int msg_count) {
+    
+    // Move all messages up by 1
+    char *first_msg = msg_array[0];
+    for (int i = 0; i < msg_count-1; i++) {
+        msg_array[i] = msg_array[i+1];
     }
-    msg_array[0] = temp_ptr;
-    memset(msg_array[0], '\0', MSGLEN);
-    //Add newest message as first
-    memcpy(msg_array[0], buf, MSGLEN);
-    return msg_array;
+    msg_array[msg_count-1] = first_msg;
+    
+    // Add newest message as first
+    memset(msg_array[msg_count-1], '\0', MSGLEN);
+    memcpy(msg_array[msg_count-1], buf, MSGLEN);
 }
 
 // This function is a starting point for a thread.
@@ -67,8 +67,8 @@ void *updateMap(void *ctx) {
 
     //Reserve memory for chat service
     message_count = map_data.height;
-    message_array = malloc(sizeof(char *) * message_count);
-    for (i = 0; i<message_count; i++) {
+    message_array = malloc(sizeof(char*) * message_count);
+    for (i = 0; i < message_count; i++) {
         message_array[i] = malloc(sizeof(char) * MSGLEN);
         memset(message_array[i], '\0', MSGLEN);
     }
@@ -87,10 +87,12 @@ void *updateMap(void *ctx) {
         else if (bytes == 0) {
             printf("thread: Server likely disconnected\n");
             break_flag = 0;
+            continue;
         }
         else if (buf[0] == 'C') {
-            printf("thread: Got C message\n");
+            printf("thread: Got C message: %s\n", buf+1);
             new_message(buf+1, message_array, message_count);
+            continue;
         }
         else if (buf[0] == 'G') {
             if ((bytes - 2) % 4 != 0) {
@@ -108,39 +110,39 @@ void *updateMap(void *ctx) {
         }
 
         // Only draw map if user isn't busy doing something else
-        pthread_mutex_lock(c->lock);
-        system("clear");
+        if (!pthread_mutex_trylock(c->lock)) {
+            system("clear");
 
-        // Add players to the map
-        for (i = 0; i < *player_count; i++) {
-            map_data.map[*y][*x] = *sign;
-            x += G_OFFSET_P;
-            y += G_OFFSET_P;
-            sign += G_OFFSET_P;
-        }
-        for (i = 0; i<map_data.height; i++) {
-            //Actual drawing
-            printf("\t%s", map_data.map[i]);
-            printf("\t\t%s\n", message_array[message_count-i-1]);
-        }
-        printf("\n");
-        x = (uint8_t*) buf+G_OFFSET_C+2;
-        y = (uint8_t*) buf+G_OFFSET_C+3;
-        // Remove players from the map
-        for (i = 0; i < *player_count; i++) {
-            map_data.map[*y][*x] = ' ';
-            x += G_OFFSET_P;
-            y += G_OFFSET_P;
-            sign += G_OFFSET_P;
-        }
+            // Add players to the map
+            for (i = 0; i < *player_count; i++) {
+                map_data.map[*y][*x] = *sign;
+                x += G_OFFSET_P;
+                y += G_OFFSET_P;
+                sign += G_OFFSET_P;
+            }
+            for (i = 0; i < map_data.height; i++) {
+                //Actual drawing
+                printf("\t%s", map_data.map[i]);
+                printf("\t\t%s\n", message_array[i]);
+            }
+            x = (uint8_t*) buf+G_OFFSET_C+2;
+            y = (uint8_t*) buf+G_OFFSET_C+3;
+            // Remove players from the map
+            for (i = 0; i < *player_count; i++) {
+                map_data.map[*y][*x] = ' ';
+                x += G_OFFSET_P;
+                y += G_OFFSET_P;
+                sign += G_OFFSET_P;
+            }
 
-        printf("DEBUG: Drawing loop: %d. Press 'h' for help\n", draw_count++);
-        pthread_mutex_unlock(c->lock);
+            printf("\nDEBUG: Drawing loop: %d. Press 'h' for help\n", draw_count++);
+            pthread_mutex_unlock(c->lock);
+        }
     }
 
     free_memory(buf);
     freeMap(&map_data);
-    
+
     // Free memory allocated for messages
     for(i = 0; i<message_count; i++) {
         free(message_array[i]);

client/update.h
@@ -9,7 +9,7 @@ void free_memory(void *);
 
 // Add the new message contained in buf to the msg_array.
 // Also rotate pointers to make the newest message show as first.
-char **new_message(char *, char **, int); 
+void new_message(char *, char **, int); 
 
 // This function is a starting point for a thread.
 // The point of this function is to read the messages received from server