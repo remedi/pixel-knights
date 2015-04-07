1954f89c8e672b88a4c76982c7afcc2096e8f939
Akbas Umur <umur.akbas@aalto.fi>
2015-04-07 21:56:59 +0300

    Tester file created and first 2 tests are implemented with help of client files thanks to Jani

---
+++ tester/reader.h
@@ -0,0 +1,19 @@
+#ifndef UPDATE_H
+#define UPDATE_H
+
+#include <signal.h>
+#include "../typedefs.h"
+
+// Thread cleanup handler
+void free_memory(void *);
+
+// Add the new message contained in buf to the msg_array.
+// Also rotate pointers to make the newest message show as first.
+void new_message(char *, char **, int); 
+
+// This function is a starting point for a thread.
+// The point of this function is to read the messages received from server
+// and to draw the game on the terminal
+void *updateMap(void *);
+
+#endif
